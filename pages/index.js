cfd8332e354da977c85e250e1b3c57573bfb8392
임찬수 <cs.lim@onul-hoi.com>
2022-03-20 11:37:13 +0900

    로딩 화면 추가 (CSR)

--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,19 @@ import { useEffect, useState } from "react";
 
 import Head from "next/head";
 import ItemList from "../src/component/ItemList";
-import { Divider, Header } from "semantic-ui-react";
+import { Divider, Header, Loader } from "semantic-ui-react";
 
 export default function Home() {
   const API_URL =
     "https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
 
   const [list, setList] = useState([]);
+  const [isLoading, setLoading] = useState(true);
 
   const getData = () => {
     axios.get(API_URL).then((res) => {
       setList(res.data);
+      setLoading(false);
     });
   };
 
@@ -26,17 +28,26 @@ export default function Home() {
       <Head>
         <title>HOME | Next.js</title>
       </Head>
-      <section className={"contents"}>
-        <Header as={"h3"}>베스트 상품</Header>
-        <Divider />
-        <ItemList list={list.slice(0, 9)} />
-      </section>
 
-      <section className={"contents"}>
-        <Header as={"h3"}>신상품</Header>
-        <Divider />
-        <ItemList list={list.slice(9)} />
-      </section>
+      {isLoading ? (
+        <Loader inline={"centered"} active>
+          ..상품 불러오는중..
+        </Loader>
+      ) : (
+        <>
+          <section className={"contents"}>
+            <Header as={"h3"}>베스트 상품</Header>
+            <Divider />
+            <ItemList list={list.slice(0, 9)} />
+          </section>
+
+          <section className={"contents"}>
+            <Header as={"h3"}>신상품</Header>
+            <Divider />
+            <ItemList list={list.slice(9)} />
+          </section>
+        </>
+      )}
     </div>
   );
 }
--- pages/view/[id].js
+++ pages/view/[id].js
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import axios from "axios";
 
 import Item from "../../src/component/Item";
+import { Loader } from "semantic-ui-react";
 
 const Post = () => {
   const router = useRouter();
@@ -10,10 +11,12 @@ const Post = () => {
   const API_URL = `https://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
 
   const [item, setItem] = useState({});
+  const [isLoading, setLoading] = useState(true);
 
   const getItemDetailData = () => {
     axios.get(API_URL).then((res) => {
       setItem(res.data);
+      setLoading(false);
     });
   };
 
@@ -23,7 +26,36 @@ const Post = () => {
     }
   }, [id]);
 
-  return <Item item={item} />;
+  return isLoading ? (
+    <Loader inline={"centered"} active>
+      ..상품 불러오는중..
+    </Loader>
+  ) : (
+    <Item item={item} />
+  );
 };
 
 export default Post;
+
+/*
+ * # Next.js 모든 페이지는 사전 렌더링 된다. (Pre-rendering)
+ * - 더 좋은 퍼포먼스
+ * - 검색엔진최적화(SEO)
+ *
+ * # Pre-rendering의 두가지 형태
+ * - 정적 생성
+ * - Server Side Rendering (SSR, Dynamic Rendering)
+ * (차이점 = 언제 html 파일을 생성하는가에 있음)
+ *
+ * [정적 생성]
+ * - 프로젝트가 빌드하는 시점에 html파일들이 생성
+ * - 모든 요청에 재사용
+ * - 퍼포먼스 이유로 Next.js는 정적 생성을 권고
+ * - 정적 생성된 페이지들은 CDN에서 캐시
+ * - getStaticProps / getStaticPaths
+ *
+ * [서버 사이드 렌더링]
+ * - 항상 최신 상태 유지
+ * - getServerSideProps
+ *
+ * */
